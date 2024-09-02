2c57cf62e085f91442a259c7db7e6e2437fcf25c
Lorenzo Giannella <l.giannella@quid.eu>
2024-09-02 13:40:47 +0000

    Completed the config to deploy WordPress

--- locals.tf
+++ locals.tf
@@ -1,1 +1,10 @@
+#defining some local variables that we will be using in a lot of places (for eg. tags).
 
+locals {
+  tags = {
+    Name = "Codio Terraform Demo"
+  }
+
+  database = "wordpress"
+  user     = "wordpress_user"
+}
